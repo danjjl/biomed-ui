b131a56e9d08eb17cd8d2a73a6a4775f37bce1d3
Jonathan Dan <danjjl@hotmail.com>
2013-02-18 21:35:10 +0100

    Variable locale en variable membre
    passé en self (garantie pas détruite à la sortie de __init()

--- newUser.py
+++ newUser.py
@@ -9,23 +9,24 @@ class newUser(QtGui.QWidget):
         super(newUser, self).__init__(parent)
 
         """Champs présent dans le formulaire"""
-        firstName = QtGui.QLineEdit() #Champs texte
-        firstName.setMaxLength(25)
+        self.firstName = QtGui.QLineEdit() #Champs texte
+        self.firstName.setMaxLength(25)
 
-        lastName = QtGui.QLineEdit() #Champs texte
-        lastName.setMaxLength(25)
+        self.lastName = QtGui.QLineEdit() #Champs texte
+        self.lastName.setMaxLength(25)
 
-        age = QtGui.QDateTimeEdit() #Champs date
-        age.setDisplayFormat("dd MMM yyyy") #ex : 24 mai 1992
-        age.setCalendarPopup(True)
+        self.age = QtGui.QDateTimeEdit() #Champs date
+        self.age.setDisplayFormat("dd MMM yyyy") #ex : 24 mai 1992
+        self.age.setCalendarPopup(True)
 
-        sexe = QtGui.QComboBox() #Liste déroulante
-        sexe.addItem("Femme")
-        sexe.addItem("Homme")
+        self.sexe = QtGui.QComboBox() #Liste déroulante
+        self.sexe.addItem("Femme")
+        self.sexe.addItem("Homme")
 
         """Layout du formulaire + label"""
-        formulaire = QtGui.QFormLayout(parent)
-        formulaire.addRow("Prenom", firstName)
-        formulaire.addRow("Nom", lastName)
-        formulaire.addRow("Age", age)
-        formulaire.addRow("Sexe", sexe)
+        self.formulaire = QtGui.QFormLayout(parent)
+        self.formulaire.addRow("Prenom", self.firstName)
+        self.formulaire.addRow("Nom", self.lastName)
+        self.formulaire.addRow("Age", self.age)
+        self.formulaire.addRow("Sexe", self.sexe)
+        setLayout(self.formulaire)
