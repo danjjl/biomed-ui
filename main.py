6f97a2539aa67d6c1a928da24bee89ea230bd2b9
Jonathan Dan <danjjl@hotmail.com>
2013-02-20 18:33:32 +0100

    Fichier séparé pour le code général, indépendant de chaque vue

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt4 import QtGui
+
+from noUserMesures import noUserMesures
+
+
+if __name__ == '__main__':
+
+    app = QtGui.QApplication(sys.argv)
+
+    main = QtGui.QWidget()
+    main.setWindowTitle("Test")
+    main.setGeometry(200,60, 500, 400)
+    noUsers = noUserMesures(main)
+    main.show()
+
+    sys.exit(app.exec_())
